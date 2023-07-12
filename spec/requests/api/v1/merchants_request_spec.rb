778715e3b599886f508b8f16d215bc3e0f693cd8
Garrett Gregor <garrett.gregor@gmail.com>
2023-07-12 08:41:21 -0600

    test: add suite 1a specs

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -4,9 +4,9 @@ describe "Merchants API" do
   it "sends a list of merchants" do
     create_list(:merchant, 3)
 
-    get '/api/v1/merchants'
+    get api_v1_merchants_path
 
-    expect(response).to be_successful
+    expect(response).to have_http_status(:ok)
 
     merchants_data = JSON.parse(response.body, symbolize_names: true)
     merchants = merchants_data[:data]
@@ -16,6 +16,7 @@ describe "Merchants API" do
     merchants.each do |merchant|
       expect(merchant).to have_key(:id)
       expect(merchant[:id].to_i).to be_an(Integer)
+      # ^^Question: Should this be written another way? Should this be coming back as an integer after serializing?
 
       expect(merchant).to have_key(:type)
       expect(merchant[:type]).to eq("merchant")
@@ -26,7 +27,8 @@ describe "Merchants API" do
       expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_a(String)
 
-      ## Enabling relationships for merchant and item serializer will make these pass
+      ## Question: should we be enabling relationships and foregoing the postman tests?
+      ## Enabling relationships for merchant and item serializer will make these pass:
       # expect(merchant).to have_key(:relationships)
       # expect(merchant[:relationships]).to be_a(Hash)
 
@@ -41,9 +43,9 @@ describe "Merchants API" do
   it "can return a one merchant" do
     merchant = create(:merchant)
 
-    get "/api/v1/merchants/#{merchant.id}"
+    get api_v1_merchant_path(merchant)
 
-    expect(response).to be_successful
+    expect(response).to have_http_status(:ok)
 
     merchant_data = JSON.parse(response.body, symbolize_names: true)
     merchant = merchant_data[:data]
@@ -74,32 +76,44 @@ describe "Merchants API" do
     merchant = create(:merchant)
     create_list(:item, 5, merchant_id: merchant.id)
 
-    get "/api/v1/merchants/#{merchant.id}/items"
+    get api_v1_merchant_items_path(merchant)
 
-    expect(response).to be_successful
+    expect(response).to have_http_status(:ok)
 
-    merchant_data = JSON.parse(response.body, symbolize_names: true)
-    merchant = merchant_data[:data]
+    merchant_items_data = JSON.parse(response.body, symbolize_names: true)
+    items = merchant_items_data[:data]
 
-    expect(merchant).to have_key(:id)
-    expect(merchant[:id].to_i).to be_an(Integer)
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id].to_i).to be_an(Integer)
 
-    expect(merchant).to have_key(:type)
-    expect(merchant[:type]).to eq("merchant")
+      expect(item).to have_key(:type)
+      expect(item[:type]).to eq("item")
 
-    expect(merchant).to have_key(:attributes)
-    expect(merchant[:attributes]).to be_a(Hash)
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes]).to be_a(Hash)
 
-    expect(merchant[:attributes]).to have_key(:name)
-    expect(merchant[:attributes][:name]).to be_a(String)
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
 
-    # expect(merchant).to have_key(:relationships)
-    # expect(merchant[:relationships]).to be_a(Hash)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
 
-    # expect(merchant[:relationships]).to have_key(:items)
-    # expect(merchant[:relationships][:items]).to be_a(Hash)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
 
-    # expect(merchant[:relationships][:items]).to have_key(:data)
-    # expect(merchant[:relationships][:items][:data]).to be_an(Array)
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+      expect(item[:attributes][:merchant_id]).to eq(merchant.id)
+
+      # expect(merchant).to have_key(:relationships)
+      # expect(merchant[:relationships]).to be_a(Hash)
+
+      # expect(merchant[:relationships]).to have_key(:items)
+      # expect(merchant[:relationships][:items]).to be_a(Hash)
+
+      # expect(merchant[:relationships][:items]).to have_key(:data)
+      # expect(merchant[:relationships][:items][:data]).to be_an(Array)
+    end
   end
 end

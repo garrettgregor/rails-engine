7dc6aedf89d29ca16b863ef525402aa24c8e63b7
Garrett Gregor <garrett.gregor@gmail.com>
2023-07-13 14:33:09 -0600

    feat: getting one item and all items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,7 +4,11 @@ module Api
   module V1
     class ItemsController < ApplicationController
       def index
-        render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
+        render json: ItemSerializer.new(Item.all)
+      end
+
+      def show
+        render json: ItemSerializer.new(Item.find(params[:id]))
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: %i[index show] do
-        resources :items, only: %i[index]
+        # namespace :merchants do
+        resources :items, only: %i[index], controller: 'merchants/items'
       end
+      resources :items, only: %i[index show]
     end
 
     # namespace :v2 do

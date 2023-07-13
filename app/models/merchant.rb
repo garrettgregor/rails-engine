30200c65b869f0d35f633c97d990b9b094dfa70f
Garrett Gregor <garrett.gregor@gmail.com>
2023-07-13 14:34:30 -0600

    refactor: add association for merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,7 +3,7 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
 
-  has_many :items
+  has_many :items#, dependent: :destroy
   # has_many :invoice_items, through: :items
   # has_many :invoices, through: :invoice_items
   # has_many :customers, through: :invoices

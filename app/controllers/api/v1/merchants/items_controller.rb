4c9c0cefe93ccdf105e84e6780c3ab6d9fb938b4
Garrett Gregor <garrett.gregor@gmail.com>
2023-07-13 14:33:59 -0600

    refactor: namespace merchant items

---
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Merchants
+      class ItemsController < ApplicationController
+        def index
+          render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
+        end
+      end
+    end
+  end
+end
